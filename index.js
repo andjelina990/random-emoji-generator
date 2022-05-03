7651b1ded1cedfb7213981132be89255b508ed29
Andjelina <astojanovic1900@gmail.com>
2022-05-03 18:44:25 +0200

    Add commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('sss');
+import chalk from 'chalk';
+
+import randomColor from 'randomcolor';
+const random = '#' + Math.floor(Math.random() * 16777216).toString(16);
+
+const row = [
+  `   ##############################
+   ###############################
+   ###############################
+   #####                    ######
+   #####                    ######
+   #####   ${random}          ######
+   #####                    ######
+   ###############################
+   ###############################
+   ###############################
+  `,
+];
+if (process.argv < 3) {
+  console.log(chalk.hex(random).bold(row));
+} else {
+  console.log(
+    chalk.hex(
+      randomColor({
+        luminosity: process.argv[3],
+        hue: process.argv[2],
+      }),
+    )(row),
+  );
+}
+// console.log(
+//   randomColor({
+//     luminosity: 'light',
+//     hue: 'blue',
+//   }),
+// );
